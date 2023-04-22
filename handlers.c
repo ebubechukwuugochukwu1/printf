565060575c8c04ee1fa82a0a63a5c978750b4c1c
ojo thomas bukola <126200583+Olubukolar@users.noreply.github.com>
2023-04-22 06:38:16 +0100

    Update handlers.c
    Updated handlers.c

--- handlers.c
+++ handlers.c
@@ -3,7 +3,7 @@
 /************************* WRITE HANDLE *************************/
 /**
  * handle_write_char - Prints a string
- * by ebubechukwuugochukwu1 and
+ * by ebubechukwuugochukwu1 and Olubukolar
  * @c: char types.
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags.
